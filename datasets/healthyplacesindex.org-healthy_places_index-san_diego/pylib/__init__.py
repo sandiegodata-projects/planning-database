5cbb0484789e01f2640208d0b3d9d75ad774b629
Eric Busboom <eric@civicknowledge.com>
2018-09-14 14:15:51 -0700

    Updateing logo, versions

--- datasets/healthyplacesindex.org-healthy_places_index-san_diego/pylib/__init__.py
+++ datasets/healthyplacesindex.org-healthy_places_index-san_diego/pylib/__init__.py
@@ -1,9 +1,8 @@
-
 from geoid.tiger import Tract as TigerTract
 from geoid.acs import AcsGeoid
 
-
 def acs_tract(v):
     
-    return TigerTract(v).convert(AcsGeoid)
+
+    return TigerTract.parse(v.zfill(11)).convert(AcsGeoid)
     
